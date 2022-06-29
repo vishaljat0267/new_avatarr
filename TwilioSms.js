72ac25ae82b02bb93681a89045be09cdb3090713
vishaljat0267 <vishaljat0267@gmail.com>
2022-06-29 11:32:00 +0530

    update

--- TwilioSms.js
+++ TwilioSms.js
@@ -1,5 +1,7 @@
-const accountSid = 'AC0dc67b3ed763baa101096a174775160f';
-const authToken = 'db27977355bf5af86051c450c908d8bf';
+const accountSid = '';
+//put the twilio id
+const authToken = '';
+//put the twilio authtoken
 const client = require('twilio')(accountSid, authToken);
 
 
@@ -8,5 +10,6 @@ exports.sendMobileSMS = async ( body , to) => {
  return await client.messages .create({
       body,
       to, 
-      from: '+12567980230' });
+      from: '' });
+      //put number
 }
